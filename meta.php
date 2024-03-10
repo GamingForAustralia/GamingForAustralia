21c9803a96cde4878b2d5d37184ef231e9ce1ede
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:57:33 +1100

    Update meta.php

--- meta.php
+++ meta.php
@@ -1,14 +1,14 @@
-<link rel="stylesheet" href="css/foundation.min.css">
-<link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
 
-<!-- For modern browsers -->
-<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/16x16.png">
-<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/32x32.png">
-<link rel="icon" type="image/png" sizes="48x48" href="img/favicon/48x48.png">
-<link rel="icon" type="image/png" sizes="64x64" href="img/favicon/64x64.png">
+    <!-- For modern browsers -->
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="img/favicon/48x48.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="img/favicon/64x64.png">
 
-<!-- For Safari on Mac OS -->
-<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <!-- For Safari on Mac OS -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
 
-<!-- For Windows -->
-<link rel="shortcut icon" href="img/favicon/favicon.ico">
+    <!-- For Windows -->
+    <link rel="shortcut icon" href="img/favicon/favicon.ico">
