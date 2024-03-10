d7af3cd589360875bd4b3f6b52be03ea9b10f487
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:05:16 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GamingForAustralia - Home</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <!-- Header -->
+    <?php include('header.php'); ?>
+
+    <!-- Content -->
+    <div class="row">
+        <div class="column">
+            <h1>Welcome to GamingForAustralia</h1>
+            <p>This is the home of our stream team.</p>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+
+    <script src="js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
