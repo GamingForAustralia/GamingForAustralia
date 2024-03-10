9f6c02aeef01a55f750545d02dc2b8fa243675c2
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:04:32 +1100

    Create about.php

---
+++ about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GamingForAustralia - About</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <!-- Header -->
+    <?php include('header.php'); ?>
+
+    <!-- Content -->
+    <div class="row">
+        <div class="column">
+            <h1>About GamingForAustralia</h1>
+            <p>This is the about page of GamingForAustralia.</p>
+            <!-- Add more information about your stream team here -->
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+
+    <script src="js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
