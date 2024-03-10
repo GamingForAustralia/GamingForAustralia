c5464596588c0f7a683977e5e15df73231fd5636
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:19:19 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="row">
         <div class="column">
-            <p>&copy; <?php echo date('Y'); ?> GamingForAustralia. All rights reserved.</p>
+            <p>&copy; 2015-<?php echo date('Y'); ?> GamingForAustralia. All rights reserved.</p>
         </div>
     </div>
 </footer>
