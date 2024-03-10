f612203c104d6317d145772176f4d08785c4574a
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:05:18 +1100

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!-- header.php -->
+<header>
+    <div class="row">
+        <div class="column">
+            <h1>GamingForAustralia</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="podcast.php">Podcast</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
