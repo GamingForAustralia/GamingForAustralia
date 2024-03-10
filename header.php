7c85f413b9bf593b5115bd25abf252bdc32426b9
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:21:51 +1100

    Update header.php

--- header.php
+++ header.php
@@ -2,14 +2,21 @@
 <header>
     <div class="row">
         <div class="column">
-            <h1>GamingForAustralia</h1>
-            <nav>
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="podcast.php">Podcast</a></li>
-                    <li><a href="contact.php">Contact</a></li>
+            <nav class="top-bar" data-topbar role="navigation">
+                <ul class="title-area">
+                    <li class="name">
+                        <h1><a href="#"><img src="img/logo.png" alt="GamingForAustralia Logo"> GamingForAustralia</a></h1>
+                    </li>
+                    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
                 </ul>
+                <section class="top-bar-section">
+                    <ul class="right">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="podcast.php">Podcast</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </section>
             </nav>
         </div>
     </div>
