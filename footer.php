cec5cbfc3ba915f925753a01070d9d340cc9b92d
gfaUnDead <contact@gfaundead.stream>
2024-03-10 15:05:20 +1100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<footer>
+    <div class="row">
+        <div class="column">
+            <p>&copy; <?php echo date('Y'); ?> GamingForAustralia. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
