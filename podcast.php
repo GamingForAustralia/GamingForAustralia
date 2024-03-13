cbba3d9edb933a333c50e0a13dacbab193d6b917
gfaUnDead <contact@gfaundead.stream>
2024-03-13 19:30:09 +1100

    Update podcast.php

--- podcast.php
+++ podcast.php
@@ -13,8 +13,11 @@
     <div class="row">
         <div class="column">
             <h1>GamingForAustralia Podcast</h1>
-            <!-- Add episodes of your podcast here -->
-            <p>No episodes available at the moment.</p>
+            <p>You can listen to our podcast, on a rolling repeat playlist here:</p>
+            <iframe src="https://relay-cloud.fourflavors.net/public/gamingforaustralia/embed?theme=dark" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 150px; border: 0;"></iframe>
+            <p>Or you may listen to any of our PodCast Episodes here:</p>
+            <iframe src="https://relay-cloud.fourflavors.net/public/gamingforaustralia/podcast/1eee112c-7c9a-6292-a115-b1115c059090?embed=true&theme=dark" frameborder="0" allowtransparency="true" style="width: 100%; height: 800px; border: 0;"></iframe>
+            <br><br><br><br><br><br>
         </div>
     </div>
 
