90734c9a3aef712822b783707b0d76b0bf22cc95
gfaUnDead <contact@gfaundead.stream>
2024-03-13 20:23:55 +1100

    Update about.php

--- about.php
+++ about.php
@@ -13,8 +13,62 @@
     <div class="row">
         <div class="column">
             <h1>About GamingForAustralia</h1>
-            <p>This is the about page of GamingForAustralia.</p>
-            <!-- Add more information about your stream team here -->
+            <p>Australian Gamers, gaming and streaming for you, right from the heart of Australia.</p>
+            <div class="grid-container">
+                <div class="grid-x grid-padding-x small-up-2 medium-up-4">
+                    <!-- Profile 1 -->
+                    <div class="cell">
+                        <div class="card">
+                            <img src="img/profile/gfaUnDead.png" alt="gfaUnDead">
+                            <div class="card-section">
+                                <h4>gfaUnDead</h4>
+                                <p>Gaming, coding and hanging out with friends, you'll see this and more, if you join the zombie horde on Twitch!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Profile 2 -->
+                    <div class="cell">
+                        <div class="card">
+                            <img src="img/profile/TheFez.png" alt="TheFez">
+                            <div class="card-section">
+                                <h4>TheFez</h4>
+                                <p>The brains, the Fez!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Profile 3 -->
+                    <div class="cell">
+                        <div class="card">
+                            <img src="img/profile/Darkness.png" alt="Darkness">
+                            <div class="card-section">
+                                <h4>FallenAngelDarkness</h4>
+                                <p>I am The Fallen Angel Darkness, without darkness there will be no light.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Profile 4 -->
+                    <div class="cell">
+                        <div class="card">
+                            <img src="img/profile/MrBanyanyaa.png" alt="MrBanyanyaa">
+                            <div class="card-section">
+                                <h4>MrBanyanyaa</h4>
+                                <p>G'Day! I'm MrBanyanyaa (pronounced like Ban-Yan-yaa)! I love to play games and hangout with friends. I especially love to mess with my friends and troll them!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Add more profiles here -->
+                    <!-- Profile 5 --><!-- 
+                    <div class="cell">
+                        <div class="card">
+                            <img src="img/profile/profile5.png" alt="Profile 5">
+                            <div class="card-section">
+                                <h4>Name</h4>
+                                <p>Description</p>
+                            </div>
+                        </div>
+                    </div>-->
+                </div>
+            </div>
         </div>
     </div>
 
